b72c1e3444b8b9adc7273eeb7e64b40d90e21282
mo7amed5hairy <mo7amed5hairy@users.noreply.github.com>
2026-07-06 14:14:09 +0300

    add eye icon to password field in register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -224,11 +224,23 @@ select.input:disabled { opacity: 0.5; cursor: not-allowed; }
       <div class="grid grid-cols-12 gap-4">
         <div class="col-span-12 md:col-span-6">
           <label class="label">كلمة المرور <span class="text-rose-500">*</span></label>
-          <input class="input" type="password" name="password" placeholder="أقل شيء 8 أحرف" required oninvalid="this.setCustomValidity('حقل كلمة المرور مطلوب')" oninput="this.setCustomValidity('')">
+          <div class="relative">
+            <input class="input pl-10" type="password" name="password" id="regPassword" placeholder="أقل شيء 8 أحرف" required oninvalid="this.setCustomValidity('حقل كلمة المرور مطلوب')" oninput="this.setCustomValidity('')">
+            <button type="button" onclick="togglePassword('regPassword', this)" class="absolute left-2 top-1/2 -translate-y-1/2 text-slate-400 hover:text-slate-600 p-1" tabindex="-1">
+              <svg class="w-5 h-5 eye-open" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/></svg>
+              <svg class="w-5 h-5 eye-closed hidden" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"/></svg>
+            </button>
+          </div>
         </div>
         <div class="col-span-12 md:col-span-6">
           <label class="label">تأكيد كلمة المرور <span class="text-rose-500">*</span></label>
-          <input class="input" type="password" name="password_confirmation" placeholder="تأكيد كلمة المرور" required oninvalid="this.setCustomValidity('يرجى تأكيد كلمة المرور المطابقة')" oninput="this.setCustomValidity('')">
+          <div class="relative">
+            <input class="input pl-10" type="password" name="password_confirmation" id="regPasswordConfirm" placeholder="تأكيد كلمة المرور" required oninvalid="this.setCustomValidity('يرجى تأكيد كلمة المرور المطابقة')" oninput="this.setCustomValidity('')">
+            <button type="button" onclick="togglePassword('regPasswordConfirm', this)" class="absolute left-2 top-1/2 -translate-y-1/2 text-slate-400 hover:text-slate-600 p-1" tabindex="-1">
+              <svg class="w-5 h-5 eye-open" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/></svg>
+              <svg class="w-5 h-5 eye-closed hidden" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"/></svg>
+            </button>
+          </div>
         </div>
       </div>
 
@@ -396,6 +408,21 @@ select.input:disabled { opacity: 0.5; cursor: not-allowed; }
         window.scrollTo({ top: 0, behavior: 'smooth' });
     });
   });
+
+  function togglePassword(inputId, btn) {
+    var input = document.getElementById(inputId);
+    var openEye = btn.querySelector('.eye-open');
+    var closedEye = btn.querySelector('.eye-closed');
+    if (input.type === 'password') {
+      input.type = 'text';
+      openEye.classList.add('hidden');
+      closedEye.classList.remove('hidden');
+    } else {
+      input.type = 'password';
+      openEye.classList.remove('hidden');
+      closedEye.classList.add('hidden');
+    }
+  }
 
   function calcAgeFromYear() {
     var selectField = document.getElementById('dateOfBirth');
